b730b108b2dd78b2346e44d0cfac4e8b4c89a670
christianc2000 <christiancelsomamanisoliz0@gmail.com>
2023-07-13 15:33:34 -0400

    first commit

---
+++ src/util/Apis.jsx
@@ -0,0 +1,26 @@
+//TODO: REFERENCIAS A ANALISIS EN AWS
+
+//CAMARA
+export const proyectSoftware="http://ec2-34-238-49-97.compute-1.amazonaws.com/api/";
+
+//CAMARA
+export const storageCamara="http://ec2-34-238-49-97.compute-1.amazonaws.com/api/rekognition/controlCamera";
+
+
+//DESCARGA
+export const storageDescarga="http://ec2-34-238-49-97.compute-1.amazonaws.com/api/rekognition/controlDownload";
+
+//FACEBOOK
+export const storageFacebook="http://ec2-34-238-49-97.compute-1.amazonaws.com/api/rekognition/controlFacebook";
+
+//TELEGRAM
+export const storageTelegram="http://ec2-34-238-49-97.compute-1.amazonaws.com/api/rekognition/storageTelegram";
+
+//LOCALIZACION
+
+//CONTACTOS
+//export const storageContacto="http://ec2-34-238-49-97.compute-1.amazonaws.com/api/storageContacto";
+
+//export const storageContacto="http://protectingyou.sw1.lol/api/storageContacto";
+
+//CAPTURAS DE PANTALLA
